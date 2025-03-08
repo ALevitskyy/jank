3e53e9f1bdd6c0eb35bf0cbf91c021be506c4ccd
Andriy Levitskyy <levitskyyandriy@gmail.com>
2025-03-08 15:33:53 -0500

    Beef up nrepl/logic.cpp

--- compiler+runtime/src/cpp/jank/nrepl/logic.cpp
+++ compiler+runtime/src/cpp/jank/nrepl/logic.cpp
@@ -16,17 +16,47 @@ namespace jank
   {
     BencodeValuePtr decoded = readBencode(input);
 
-    if(std::holds_alternative<std::string>(*decoded))
+    if(std::holds_alternative<std::map<std::string, BencodeValuePtr>>(*decoded))
     {
-      std::string line = std::get<std::string>(*decoded);
-      boost::trim(line);
-      std::string result = evaluate_code(line, path);
-      BencodeValuePtr encoded = std::make_shared<BencodeValue>(result);
-      writeBencode(encoded, output);
+      auto const &map = std::get<std::map<std::string, BencodeValuePtr>>(*decoded);
+      auto it = map.find("op");
+      if(it != map.end() && std::holds_alternative<std::string>(*it->second))
+      {
+        std::string op = std::get<std::string>(*it->second);
+        if(op == "eval")
+        {
+          auto code_it = map.find("code");
+          if(code_it != map.end() && std::holds_alternative<std::string>(*code_it->second))
+          {
+            std::string code = std::get<std::string>(*code_it->second);
+            boost::trim(code);
+            std::string result = evaluate_code(code, path);
+            BencodeValuePtr encoded = std::make_shared<BencodeValue>(result);
+            writeBencode(encoded, output);
+          }
+          else
+          {
+            BencodeValuePtr encoded = std::make_shared<BencodeValue>("incorrect format");
+            writeBencode(encoded, output);
+          }
+        }
+        else
+        {
+          BencodeValuePtr encoded
+            = std::make_shared<BencodeValue>("only eval key is supported for now");
+          writeBencode(encoded, output);
+        }
+      }
+      else
+      {
+        BencodeValuePtr encoded = std::make_shared<BencodeValue>("incorrect format");
+        writeBencode(encoded, output);
+      }
     }
     else
     {
-      std::cerr << "Received non-string bencoded value" << std::endl;
+      BencodeValuePtr encoded = std::make_shared<BencodeValue>("incorrect format");
+      writeBencode(encoded, output);
     }
   }
 }
